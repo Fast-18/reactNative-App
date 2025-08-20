d0b5c8ee748a62ad25e9540b831492d22ccfe325
MATHEUS HENRIQUE CARDOSO KRUTLI <krutli.4961@aluno.pr.senac.br>
2025-08-20 09:01:25 -0300

    Colocando botão para entrar e terminar o rodapé da tela, finalizando o front-end da tela inicial

--- src/pages/login/index.tsx
+++ src/pages/login/index.tsx
@@ -1,48 +1,51 @@
 import React from 'react';
-import { Text, View, Image, TextInput} from "react-native";
-import { style } from "./styles";
+import { Text, View, Image, TextInput, TouchableOpacity } from 'react-native';
+import { style } from './styles';
 import Logo from "../../assets/logo.png";
 import {MaterialIcons} from "@expo/vector-icons";
 import { themas } from '../../global/themes';
 
 export default function Login() {
-    return (
-    < View style = {style.container} >
-        <View style={style.boxTop}>
-            <Image
-        source={Logo}
-        style={style.logo}
-        resizeMode="contain"
-            />
-            <Text>Bem viindo de volta!</Text>
-            <Text style={style.text}>Bem vindo de volta!</Text>
-        </View>
-        <View style={style.boxMid}>
-            <Text style={style.titleInput}>ENDEREÇO DE E-MAIL</Text>
-            <View style={style.boxInput}>
-            <TextInput
-                style={style.input}
-            />
-            <MaterialIcons
-            name='email' 
-            size={20}
-            color={themas.colors.gray}
-            />
-            </View>
-            <Text style={style.titleInput}>SENHA</Text>
-            <View style={style.boxInput}/>
-            <TextInput 
+  return (
+    <View style={style.container}>
+      <View style={style.boxTop}>
+        <Image
+          source={Logo}
+          style={style.logo}
+          resizeMode="contain"
+        />
+        <Text style={style.text}>Bem-Vindo de Volta</Text>
+      </View>
+      <View style={style.boxMid}>
+        <Text style={style.titleInput}>Endereço de E-mail</Text>
+        <View style={style.boxInput}>
+          <TextInput
             style={style.input}
-            />
-            <MaterialIcons
-            name="remove-red-eye"
-            size={20}
-            color={themas.colors.gray}
-            />
-            </View>
-            <View style={style.boxBotton}>
-            </View>
+          />
+          <MaterialIcons 
+          name = "email"
+          size={20}
+          color={themas.colors.gray}
+          />
         </View>
- 
-    )
+        <Text style={style.titleInput}>Senha</Text>
+        <View style={style.boxInput}>
+          <TextInput
+          style={style.input}
+          />
+          <MaterialIcons 
+          name = "remove-red-eye"
+          size={20}
+          color={themas.colors.gray}
+          />
+        </View>
+      </View>
+      <View style={style.boxBotton}>
+        <TouchableOpacity style={style.button}>
+          <Text style={style.textButton}>Entrar</Text>
+        </TouchableOpacity>
+      </View>
+      <Text style={style.textBotton}>Não tem conta? <Text style={{color: themas.colors.primary}}>Crie Agora!</Text></Text>
+    </View>
+  );
 }
--- src/pages/login/styles.ts
+++ src/pages/login/styles.ts
@@ -1,41 +1,40 @@
-import { StyleSheet } from "react-native";
+import { Button, StyleSheet } from "react-native";
 import { Dimensions } from "react-native";
 import { themas } from "../../global/themes";
-
 
 export const style = StyleSheet.create ({
     container: {
         flex: 1,
         alignItems: 'center',
         justifyContent: 'center',
-       // padding: 20
+        //padding: 20
     },
     boxTop: {
         height: Dimensions.get('window').height / 3,
         width: '100%',
-       // backgroundColor: 'red'
-       alignItems: 'center',
-       justifyContent: 'center'
+        //backgroundColor: 'red'
+        alignItems: 'center',
+        justifyContent: 'center'
     },
     boxMid: {
         height: Dimensions.get('window').height / 4,
         width: '100%',
-       // backgroundColor: 'green',
-       paddingHorizontal: 37,
+        //backgroundColor: 'green',
+        paddingHorizontal: 37
     },
     boxBotton: {
         height: Dimensions.get('window').height / 3,
         width: '100%',
-        //backgroundColor: 'blue',
-        borderWidth: 1,
+        //backgroundColor: 'blue'
+        alignItems: 'center',
     },
     logo: {
-       width: 80,
-       height: 80, 
+        width: 80,
+        height: 80
     },
     text: {
         fontWeight: 'bold',
-        marginLeft: 48,
+        marginTop: 48,
         fontSize: 18
     },
     titleInput: {
@@ -57,8 +56,33 @@ export const style = StyleSheet.create ({
     },
     input: {
         height: '100%',
-        width: '90%',
+        width: "90%",
         //backgroundColor: 'red',
         borderRadius: 40
+    },
+    button: {
+        width: 250,
+        height: 50,
+        alignItems: 'center',
+        justifyContent: 'center',
+        backgroundColor: themas.colors.primary,
+        borderRadius: 40,
+        shadowColor: "#000",
+        textShadowOffset: {
+            width: 0,
+            height: 3,
+        },
+        shadowOpacity: 0.29,
+        shadowRadius: 4.65,
+        elevation: 7,
+    },
+    textButton: {
+        fontSize: 16,
+        color: '#fff',
+        fontWeight: 'bold'
+    },
+    textBotton: {
+        fontSize: 16,
+        color: themas.colors.gray
     }
 })
