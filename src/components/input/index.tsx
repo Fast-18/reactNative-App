9bdbb0a5658f2e716abc26ba6d7365d781710522
MATHEUS HENRIQUE CARDOSO KRUTLI <krutli.4961@aluno.pr.senac.br>
2025-09-01 14:31:58 -0300

    criando style dentro de components e comecando a componetizaçao do codigo

--- src/components/input/index.tsx
+++ src/components/input/index.tsx
@@ -1,22 +1,39 @@
-import React, { forwardRef } from "react";
-import { View, Text, TextInput } from "react-native";
+import React, { forwardRef, Fragment} from "react";
+import { View, Text, TextInput, TextInputProps } from "react-native";
 import { style } from "../../pages/login/styles"; 
-import { MaterialIcons}  from '@expo/vector-icons';
+//import { MaterialIcons}  from '@expo/vector-icons';
 import { themas } from "../../global/themes";
+import { FontAwesome, MaterialIcons, Octicons } from "@expo/vector-icons";
+
+type IconComponent = React.ComponentType<React.ComponentProps<typeof MaterialIcons>> |
+React.ComponentType<React.ComponentProps<typeof FontAwesome>> |
+React.ComponentType<React.ComponentProps<typeof Octicons>>;
+
+type Props = TextInputProps & {
+    Iconleft?: IconComponent,
+    IconRight?: IconComponent,
+    IconLeftName?: string,
+    IconRightName?: string,
+    title?: string,
+    OnIconLeftPress?: () => void,
+    OnIconRightPress?: () => void
+}
 
 export const Input =  forwardRef (() => {
     return (
-         <View style={style.boxInput}>
-                  <TextInput
-                    style={style.input}
-                 
-                    
-                  />
-                  <MaterialIcons 
-                  name="email" 
-                  size={20} 
-                  color={themas.colors.gray} />
-                </View>
+        <>
+        <Text style={style.titleInput}>ENDEREÇO DE E-MAIL</Text>
+        <View style={style.boxInput}>
+            <TextInput
+                style={style.input}
+                />
+                <MaterialIcons
+                name="email"
+                size={20}
+                color={themas.colors.gray}
+                />
+        </View>
+        </>
     )
 
 })
