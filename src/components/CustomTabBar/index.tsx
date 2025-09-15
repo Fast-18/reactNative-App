45066f3c4cd126a4125951c7d6dc39d89756f0d4
MATHEUS HENRIQUE CARDOSO KRUTLI <krutli.4961@aluno.pr.senac.br>
2025-09-15 12:17:36 -0300

    Arrumando tabItemButton, deixando ele centralizado e mexendo tambem na opacidade do bottom

--- src/components/CustomTabBar/index.tsx
+++ src/components/CustomTabBar/index.tsx
@@ -6,17 +6,25 @@ import { AntDesign, FontAwesome, Entypo, MaterialIcons} from '@expo/vector-icons
 export default ({ state, navigation }: any) => {
     return (
         <View style={style.tabArea}>
-            <TouchableOpacity style={style.tabArea}>
+            <TouchableOpacity style={style.tabItem}>
                <AntDesign
                name="bars"
                style={{fontSize: 32}}
                />
             </TouchableOpacity>
             <TouchableOpacity style={style.tabItemButton}>
-                <View>
+                <View style={{ width: '100%', left: 10, top: 4}}>
                     <Entypo
                     name="plus"
                     size={40}
+                    color={'#FFF'}
+                    />
+                </View>
+                <View style={{flexDirection: 'row-reverse', width: '100%', right: 10, bottom: 10}}>
+                    <MaterialIcons
+                    name="edit"
+                    style={{color: '#FFF'}}
+                    size={30}
                     />
                 </View>
             </TouchableOpacity>
