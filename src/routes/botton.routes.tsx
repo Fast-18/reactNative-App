5611b0e5049592cf71e1b556bfd220becfde81d9
MATHEUS HENRIQUE CARDOSO KRUTLI <krutli.4961@aluno.pr.senac.br>
2025-09-17 08:58:31 -0300

    criando AuthContext, aplicando modal nas duas telas de list e user

--- src/routes/botton.routes.tsx
+++ src/routes/botton.routes.tsx
@@ -2,11 +2,13 @@ import { createBottomTabNavigator } from "@react-navigation/bottom-tabs";
 import List from "../pages/list";
 import User from "../pages/user";
 import CustomTabBar from "../components/CustomTabBar";
+import { authProviderList } from "../context/authContextext_list";
 
 const Tab = createBottomTabNavigator();
 
 export default function BottomRoutes() {
     return (
+        <authProviderList>
         <Tab.Navigator
             screenOptions={{
                 headerShown: false
@@ -23,5 +25,6 @@ export default function BottomRoutes() {
             />
 
         </Tab.Navigator>
+        </authProviderList>
     )
 }
