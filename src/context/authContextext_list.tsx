e2c23de84f71c0b0279954b0cef623c08ca591d5
MATHEUS HENRIQUE CARDOSO KRUTLI <krutli.4961@aluno.pr.senac.br>
2025-10-02 07:53:10 -0300

    Salvando arrays no banco de dados via terminal, criando tasklist e salvando como string

--- src/context/authContextext_list.tsx
+++ src/context/authContextext_list.tsx
@@ -83,7 +83,10 @@ export const AuthProviderList = (props: any): any => {
                     selectedTime.getMinutes()
                 ).toISOString(),
             }
-
+            const storageData = await AsyncStorage.getItem('tasklist');
+            console.log(storageData)
+            let taskList = storageData ? JSON.parse(storageData) : [];
+            taskList.push(newItem)
             await AsyncStorage.setItem('tasklist', JSON.stringify(newItem))
 
         } catch (error) {
