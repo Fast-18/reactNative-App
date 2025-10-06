6f5b8714958e11f865a3d44ef98c10538e29d8f1
MATHEUS HENRIQUE CARDOSO KRUTLI <krutli.4961@aluno.pr.senac.br>
2025-10-06 12:03:39 -0300

    renderizando tarefas, aplicando data do formato BR alem de criar novas propriedades para salvar no banco de dados

--- src/global/Props.tsx
+++ src/global/Props.tsx
@@ -1,12 +1,14 @@
-interface AuthContextType {
+export interface AuthContextType {
     taskList: Array<PropCard>,
     onOpen: void,
 
-}type PropCard = {
+}
+
+export type PropCard = {
     description: string,
     flag: PropFlags,
     item: number,
     timeLimit: string,
     title: string,
 }
-type PropFlags = 'Urgente' | 'Opcional'
+export type PropFlags = 'Urgente' | 'Opcional'
