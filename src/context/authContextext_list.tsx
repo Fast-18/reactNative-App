2a647504911a5611a9c8bf0b8c4d562fb604b21a
MATHEUS HENRIQUE CARDOSO KRUTLI <krutli.4961@aluno.pr.senac.br>
2025-10-06 11:16:54 -0300

    Criando get_taskList no useEffect para renderizar na hora e tarefa criada e quando abrir o App aparece o historico das tarefas adicionadas

--- src/context/authContextext_list.tsx
+++ src/context/authContextext_list.tsx
@@ -43,8 +43,8 @@ export const AuthProviderList = (props: any): any => {
     }
 
     useEffect(() => {
-        console.log(taskList.length)
-    }, [taskList]);
+        get_taskList()
+    }, []);
 
     const _renderFlags = () => {
         return (
@@ -110,6 +110,18 @@ export const AuthProviderList = (props: any): any => {
             setItem(0)
            setSelectedDate(new Date())
            setSelectedTime(new Date())
+        }
+
+        async function get_taskList() {
+            try {
+                const storageData = await AsyncStorage.getItem('taskList');
+                const taskList
+                 = storageData ? JSON.parse(storageData) : []
+                 setTaskList(taskList)
+            }catch (error) {
+                console.log(error )
+            }
+
         }
 
     const _container = () => {
